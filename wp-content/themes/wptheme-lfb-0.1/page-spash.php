c7a582a228085b4e8462e396e1746f6dd04b7718
Matthew Seccafien <matt@studiocartogram.com>
2013-01-28 17:38:25 -0500

    added splash page for production site.
    Signed-off-by: Matthew Seccafien <matt@studiocartogram.com>

---
+++ wp-content/themes/wptheme-lfb-0.1/page-spash.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Template Name: Splash Page
+ */
+?>
+<?php get_template_part('parts/shared/html-header'); ?>
+<div id="modal-splash" class="reveal-modal transparent">
+  <h2>Left Field is Coming Soon.</h2>
+ </div>
+
+<?php get_template_part('parts/shared/html-footer'); ?>
