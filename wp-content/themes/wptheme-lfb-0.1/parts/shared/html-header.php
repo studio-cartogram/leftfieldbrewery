704f300365873a4abf55cb01491806da5a118a99
Matthew Seccafien <matt@studiocartogram.com>
2013-02-03 18:34:52 -0500

    added meta description
    Signed-off-by: Matthew Seccafien <matt@studiocartogram.com>

--- wp-content/themes/wptheme-lfb-0.1/parts/shared/html-header.php
+++ wp-content/themes/wptheme-lfb-0.1/parts/shared/html-header.php
@@ -9,6 +9,7 @@
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	  	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0"><!-- Remove if you're not building a responsive site. (But then why would you do such a thing?) -->
+		<meta name="description" content="<?php bloginfo('description') ?>">
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 		<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon.ico"/>
 		<script type="text/javascript" src="//use.typekit.net/ekm7axz.js"></script>
