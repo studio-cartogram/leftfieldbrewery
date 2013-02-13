24831c523ed32319ac3318d634d350c6a00f895a
Peter Tran <pete.tran02@gmail.com>
2013-02-13 18:21:38 -0500

    Removed loading content in the loop.
    Signed-off-by: Peter Tran <pete.tran02@gmail.com>

--- wp-content/themes/wptheme-lfb-0.1/parts/loops/loop.php
+++ wp-content/themes/wptheme-lfb-0.1/parts/loops/loop.php
@@ -8,18 +8,14 @@ $posts->query( array(
 	if ($posts->have_posts()) : ?>	
 		<?php while ($posts->have_posts()) : $posts->the_post(); ?>
 		<li>
-<<<<<<< HEAD
 			<div class="row slide-row">
 				<div class="columns twelve">
-					<?php //get_template_part('parts/content/content', get_post_format() ); ?>	
+
+					<?php $slug = basename(get_permalink()); 
+					//get_template_part('parts/content/content', $slug ); ?>
 				</div>
 			</div>
-=======
 			
-					<?php $slug = basename(get_permalink()); 
-					get_template_part('parts/content/content', $slug ); ?>	
-				
->>>>>>> 94eccba856f142ce481ba1ce1433cb0e81c856bb
 		</li>		
 		<?php endwhile; ?>	
 	<?php endif; 
