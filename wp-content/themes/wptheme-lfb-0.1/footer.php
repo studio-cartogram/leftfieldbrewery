b99981697189a76c245df730d1cf86353a18c773
Peter Tran <pete.tran02@gmail.com>
2013-02-15 20:11:05 -0500

    Started footer: Need latest tweet, links to social media, setup newsletter plugin
    Signed-off-by: Peter Tran <pete.tran02@gmail.com>

--- wp-content/themes/wptheme-lfb-0.1/footer.php
+++ wp-content/themes/wptheme-lfb-0.1/footer.php
@@ -1,23 +1,38 @@
-<?php $options = get_option('rsof_theme_options');?> 
+<?php $options = get_option('lfb_theme_options');?> 
 <section class="container" id="container-footer">
 	<div class="row">
-		<div class="columns four hook">
-			<ul class="no-bullet text-small">
-				<li><?php echo $options['address']; ?></li>
-				<li><?php echo $options['address2']; ?></li>
-			</ul>	
+		<div class="columns six hook">
+			<?php gravity_form('Newsletter', false, false, false, '', true, 300); ?>
 		</div>
-		<div class="columns four">
-			<ul class="no-bullet text-small">
-				<li><?php echo $options['office_phone']; ?></li>
-				<li><?php echo $options['mobile_phone']; ?></li>
-			</ul>	
+		<div class="columns three">
+			<div class="row">
+				<div class="columns twelve">
+					<h4>Get in touch</h4>
+					<ul class="no-bullet text-small">
+						<li><?php echo '<a href="mailto:'. $options['email'] . '">' . $options['email'] . '</a>' ; ?></li>
+						<li><?php echo $options['phone']; ?></li>
+					</ul>	
+				</div>
+			</div>
+			<div class="row">
+				<div class="columns twelve">
+					<h4>Share</h4>
+					<?php $globalnav = array(
+						'theme_location'  => 'social',
+						'container'       => 'false', 
+						'menu_class'      => 'nav-skew', 
+						'menu_id'         => 'nav-social',
+						'echo'            => true,
+						'fallback_cb'     => ''
+						); 
+						wp_nav_menu( $globalnav ); 
+					?>
+				</div>
+			</div>
 		</div>
-		<div class="columns four end">
-			<ul class="no-bullet text-small">
-				<li><?php echo '<a href="mailto:'. $options['email'] . '">' . $options['email'] . '</a>' ; ?></li>
-				<li><?php echo '<a href="mailto:'. $options['email2'] . '">' . $options['email2'] . '</a>' ; ?></li>
-			</ul>	
+		<div class="columns three">
+			<h4>Chatter</h4>
+			<?php the_widget("TTrust_Twitter"); ?>
 		</div>
 	</div>
 </section>
