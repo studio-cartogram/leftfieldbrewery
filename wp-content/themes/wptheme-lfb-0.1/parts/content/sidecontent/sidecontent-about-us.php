c3d7fe5e319862840e94d2eb658016525f6834e0
Peter Tran <pete.tran02@gmail.com>
2013-02-16 16:28:28 -0500

    Started displays cards in about-me
    Signed-off-by: Peter Tran <pete.tran02@gmail.com>

--- wp-content/themes/wptheme-lfb-0.1/parts/content/sidecontent/sidecontent-about-us.php
+++ wp-content/themes/wptheme-lfb-0.1/parts/content/sidecontent/sidecontent-about-us.php
@@ -1,1 +1,15 @@
-<h2>TestM</h2>
+<?php
+$players = new WP_Query( array( "post_type" => "players") );
+?>
+<h6>Meet the team</h6>
+<div class="playersFlexSlider">
+	<ul class="slides">
+		<?php while ($players->have_posts()) : $players->the_post(); ?>
+			<li class="playerCard">
+				<?php the_post_thumbnail();?>
+				<?php $email = get_post_meta( $post->ID, '_cartogram_email_value', TRUE );?>
+				<a href="">Contact <?php the_title() ?></a>	
+			</li>
+		<?php endwhile; ?>	
+	</ul>
+</div>
