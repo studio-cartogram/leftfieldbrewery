0da2a7d54a9a5a911c8c7c7dd30547d77ef5af00
Peter Tran <pete.tran02@gmail.com>
2013-02-17 12:19:42 -0500

    Testing lowercase only for get template part
    Signed-off-by: Peter Tran <pete.tran02@gmail.com>

--- wp-content/themes/wptheme-lfb-0.1/parts/content/content.php
+++ wp-content/themes/wptheme-lfb-0.1/parts/content/content.php
@@ -17,7 +17,7 @@ $page_title = str_replace(" ", "-", get_the_title());
 <div class="row collapse">
 	<div class="columns four">
 		<?php echo $page_title ?>
-		<?php get_template_part('parts/content/sidecontent/sidecontent', $page_title);?>
+		<?php get_template_part('parts/content/sidecontent/sidecontent', strtolower($page_title));?>
 	</div>
 	<div class="columns eight">	
 		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
