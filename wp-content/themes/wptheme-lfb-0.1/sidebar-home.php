79a3c92b4b994463c7340c0f07142c9be1dbc5b1
Peter Tran <pete.tran02@gmail.com>
2013-02-22 13:35:03 -0500

    Looped through vendors and display icons
    Signed-off-by: Peter Tran <pete.tran02@gmail.com>

--- wp-content/themes/wptheme-lfb-0.1/sidebar-home.php
+++ wp-content/themes/wptheme-lfb-0.1/sidebar-home.php
@@ -1,27 +1,27 @@
 <?php
-	$establishments = new WP_Query( array( "post_type" => "establishments") );
+	$vendors = new WP_Query( array( "post_type" => "vendors") );
 
 ?>
 <div class="row">
 	<div class="columns twelve">
 		<h6>Find out beer at these all-star establishments. </h6>
-		<ul class="establishments"> 
-			<?php while ($establishments->have_posts()) : $establishments->the_post(); ?>
-				<li class="establishment">
+		<ul class="vendors"> 
+			<?php while ($vendors->have_posts()) : $vendors->the_post(); ?>
+				<li class="vendor">
 					<article class="content">
-						<?php $icons = array(	
-							"glass_icon" => get_post_meta( $post->ID, '_cartogram_bar_value', TRUE ),
-							"restaurant_icon" => get_post_meta( $post->ID, '_cartogram_restaurant_value', TRUE ),
-							"brew_pub_icon" => get_post_meta( $post->ID, '_cartogram_brew_pub_value', TRUE )
-						); ?>
-						<?php foreach ($icons as $type => $value) {
-							if ($value) {
-								//This implies the establishment has "type"={food, drinks, draft}
-								echo $type;
-							}
-						}?>
-						<?php the_title('<h7 class="establishment-name">','</h7>') ?>
-						<?php the_content() ?>				
+						<?php
+							$taxonomy_icons = array("bar"=> "&#xe016;", "restaurant" => "&#xe007;", "brew pub" => "&#xe014;");
+							$map_url = get_post_meta( $post->ID, '_cartogram_map_value', TRUE );
+							$descriptive_address = get_post_meta( $post->ID, '_cartogram_address_value', TRUE );
+							$taxonomies = wp_get_post_terms($post->ID, 'type');
+							foreach ($taxonomies as $taxonomy) {
+						?>
+								<div class="fs1"aria-hidden="true" data-icon="<?php echo $taxonomy_icons[$taxonomy->name];?>"></div>
+						
+					 		<?php }	?>
+
+						<?php the_title('<h7 class="vendor-name">','</h7>') ?>
+						<?php echo $descriptive_address;?>
 					</article>
 				</li>
 			<?php endwhile; ?>	
