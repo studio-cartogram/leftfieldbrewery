606d0d8caba5c125bd9eb55b03a4ea4ff47baba1
Fiona McDougall <fiona@studiocartogram.com>
2013-03-01 19:58:57 -0500

    re-factored icomoon font

--- resources/icomoon/lte-ie7.js
+++ resources/icomoon/lte-ie7.js
@@ -6,42 +6,47 @@ window.onload = function() {
 		el.innerHTML = '<span style="font-family: \'icomoon\'">' + entity + '</span>' + html;
 	}
 	var icons = {
-			'icon-phone' : '&#xe000;',
-			'icon-twitter' : '&#xe001;',
+			'icon-twitter' : '&#xe000;',
+			'icon-facebook' : '&#xe001;',
 			'icon-google-plus' : '&#xe002;',
-			'icon-facebook' : '&#xe003;',
-			'icon-envelope-alt' : '&#xf0e0;',
-			'icon-map-marker' : '&#xf041;',
-			'icon-bubbles' : '&#xe01f;',
-			'icon-bubbles-2' : '&#xe01e;',
-			'icon-untappd' : '&#xe004;',
-			'icon-brewpub' : '&#xe005;',
-			'icon-bar' : '&#xe006;',
-			'icon-toronto' : '&#xe007;',
-			'icon-tap' : '&#xe008;',
-			'icon-bar-restaurant' : '&#xe009;',
-			'icon-arrow-right' : '&#xe00a;',
-			'icon-restaurant' : '&#xe00b;',
-			'icon-newletter' : '&#xe00c;',
-			'icon-arrow-left' : '&#xe00d;',
-			'icon-doubleipa' : '&#xe00e;',
-			'icon-mark' : '&#xe00f;',
-			'icon-maris' : '&#xe010;',
-			'icon-mail' : '&#xe011;',
-			'icon-logo' : '&#xe012;',
-			'icon-instagram' : '&#xe013;',
-			'icon-flip-right' : '&#xe014;',
-			'icon-flip-left' : '&#xe015;',
-			'icon-est2013' : '&#xe016;',
-			'icon-eephus' : '&#xe017;',
-			'icon-diamond' : '&#xe018;',
-			'icon-diamond-stroke' : '&#xe019;',
-			'icon-cross' : '&#xe01a;',
-			'icon-close' : '&#xe01b;'
+			'icon-phone' : '&#xe003;',
+			'icon-wordmark' : '&#xe004;',
+			'icon-untappd' : '&#xe006;',
+			'icon-tap' : '&#xe007;',
+			'icon-restaurant' : '&#xe008;',
+			'icon-newletter' : '&#xe005;',
+			'icon-mark' : '&#xe009;',
+			'icon-mail' : '&#xe00a;',
+			'icon-logo' : '&#xe00b;',
+			'icon-loader-simple' : '&#xe00c;',
+			'icon-instagram' : '&#xe00d;',
+			'icon-flip-right' : '&#xe00e;',
+			'icon-flip-left' : '&#xe00f;',
+			'icon-cross' : '&#xe010;',
+			'icon-close' : '&#xe011;',
+			'icon-brewpub' : '&#xe012;',
+			'icon-bar' : '&#xe013;',
+			'icon-bar-restaurant' : '&#xe014;',
+			'icon-arrow-right' : '&#xe015;',
+			'icon-arrow-left' : '&#xe016;',
+			'icon-mark-mug' : '&#xe017;',
+			'icon-mandie-wheat' : '&#xe018;'
 		},
 		els = document.getElementsByTagName('*'),
 		i, attr, html, c, el;
 	for (i = 0; i < els.length; i += 1) {
+		el = els[i];
+		attr = el.getAttribute('data-icon');
+		if (attr) {
+			addIcon(el, attr);
+		}
+		c = el.className;
+		c = c.match(/icon-[^\s'"]+/);
+		if (c && icons[c[0]]) {
+			addIcon(el, icons[c[0]]);
+		}
+	}
+};ength; i += 1) {
 		el = els[i];
 		attr = el.getAttribute('data-icon');
 		if (attr) {
