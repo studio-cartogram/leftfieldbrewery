49bd991a58a920fb36d27dcfeb81b174c92096fe
Matthew Seccafien <matt@studiocartogram.com>
2013-03-11 09:22:16 -0400

    changed pagination single direction
    Signed-off-by: Matthew Seccafien <matt@studiocartogram.com>

--- wp-content/themes/wptheme-lfb-0.1/parts/navigation/pagination-single.php
+++ wp-content/themes/wptheme-lfb-0.1/parts/navigation/pagination-single.php
@@ -9,34 +9,34 @@ global $post;
 
 <nav class="row collapse flushed-left border-top border-bottom" id="pagination" role="navigation">
 	<div class="columns mobile-one one rule-right bg-navy pagination-arrow">
-		<?php if(!$prevURL){
+		<?php if(!$nextURL){
 				echo "<a class='cartogram-slider-disabled'><i class='icon-arrow-left'></i></a>";
  			} else { 
- 				echo '<a href="' . $prevURL . '" ><i class="icon-arrow-left"></i></a>';
+ 				echo '<a href="' . $nextURL . '" ><i class="icon-arrow-left"></i></a>';
  			}
  		?>
 	</div>
 	<div class="columns mobile-one five rule-right border-bottom bg-cream pagination-link">
-		<?php if(!$prevURL){
+		<?php if(!$nextURL){
 				echo "<a class='disabled'>Older</a>";
- 			} else {
- 				echo '<a href="' . $prevURL . '" >Older</a>';
- 			}
- 		?>
-	</div>
-	<div class="columns mobile-one five rule-right border-bottom bg-cream pagination-link">
-			<?php if(!$nextURL){
-				echo "<a class='disabled'>Newer</a>";
  			} else {
  				echo '<a href="' . $nextURL . '" >Newer</a>';
  			}
  		?>
 	</div>
+	<div class="columns mobile-one five rule-right border-bottom bg-cream pagination-link">
+			<?php if(!$prevURL){
+				echo "<a class='disabled'>Newer</a>";
+ 			} else {
+ 				echo '<a href="' . $prevURL . '" >Older</a>';
+ 			}
+ 		?>
+	</div>
 	<div class="columns mobile-one one rule-right rule-left bg-navy pagination-arrow">
-			<?php if(!$nextURL){
+			<?php if(!$prevURL){
 				echo "<a class='cartogram-slider-disabled'><i class='icon-arrow-right'></i></a>";
  			} else {
- 				echo '<a href="' . $nextURL . '" ><i class="icon-arrow-right"></i></a>';
+ 				echo '<a href="' . $prevURL . '" ><i class="icon-arrow-right"></i></a>';
  			}
  		?>
 	</div>
