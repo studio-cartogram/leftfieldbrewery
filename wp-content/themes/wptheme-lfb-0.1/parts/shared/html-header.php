2b152ddf9891dbff7609e5af2f44abe88c2585e1
Fiona McDougall <fiona@studiocartogram.com>
2013-03-13 15:55:39 -0400

    styling changes to browser updater

--- wp-content/themes/wptheme-lfb-0.1/parts/shared/html-header.php
+++ wp-content/themes/wptheme-lfb-0.1/parts/shared/html-header.php
@@ -37,7 +37,7 @@
 	<body <?php body_class(); ?>>
 	<!--[if lt IE 9]>
 	    <div class="browseralert">
-	   		<div class="woocommerce_message">
+	   		<div class="leftfield_message">
 	   	 		<p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
 	   		</div>
 	   	</div>
