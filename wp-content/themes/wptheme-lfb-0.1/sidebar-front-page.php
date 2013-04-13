d6d20dfb9a56cffbaa96f4c528b01df330350771
Matthew Seccafien <matt@studiocartogram.com>
2013-04-13 18:30:03 -0400

    fixed scroll issue on sidebar
    Signed-off-by: Matthew Seccafien <matt@studiocartogram.com>

--- wp-content/themes/wptheme-lfb-0.1/sidebar-front-page.php
+++ wp-content/themes/wptheme-lfb-0.1/sidebar-front-page.php
@@ -1,9 +1,13 @@
 <h3 class="rule-left mobile-divide text-center">Try LeftField </h3>
-<section class="vendor-listing bg-cream">		
+<section class="rule-left  vendor-listing bg-cream ">		
+	<div class="screen">
 	<p class="lead"><i class="icon-tap"></i>Find our beer at these all-star establishments.</p>
-	<ul>
-		<?php get_template_part('parts/loops/loop', 'vendor') ?>
-	</ul>
+	<div class="scrollshadow">
+		<ul>
+			<?php get_template_part('parts/loops/loop', 'vendor') ?>
+		</ul>
+	</div>	
+</div>
 	<div class="row collapse flushed-right">
 		<div class="columns twelve double-bordered">
 			<?php $options = get_option('lfb_theme_options');?> 
