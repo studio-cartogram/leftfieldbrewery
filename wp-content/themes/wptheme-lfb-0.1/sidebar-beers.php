0e0849514c876e152ad532df2a98e6bfc528a8ba
Fiona McDougall <fiona@studiocartogram.com>
2014-08-31 03:09:09 -0400

    reverting

--- wp-content/themes/wptheme-lfb-0.1/sidebar-beers.php
+++ wp-content/themes/wptheme-lfb-0.1/sidebar-beers.php
@@ -13,9 +13,7 @@
 							<?php
 							the_title('<h2 class="light beer-name beer-block">', '</h2>');
 							echo '<h3 class="light beer-tagline">' . get_post_meta( $id, '_cartogram_short_description_value', TRUE ) . '</h3>';
-							echo '<div class="back-logo big-icon">';
-							echo '<object type="image/svg+xml" data="'. bloginfo('template_directory').'/images/'. $icon .'.svg>Your browser does not support SVG</object>';
-							echo '</div>';
+							echo '<div class="back-logo big-icon icon-' . $icon . '-a"></div>';
 							the_content();
 						?>
 					</div>
