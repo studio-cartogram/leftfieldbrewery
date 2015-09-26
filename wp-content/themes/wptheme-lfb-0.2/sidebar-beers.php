3d05249499775e5750086c0bb7a2483887556c7d
Matt Seccafien <matt@studiocartogram.com>
2015-09-25 22:52:01 -0400

    added new icon size

--- wp-content/themes/wptheme-lfb-0.2/sidebar-beers.php
+++ wp-content/themes/wptheme-lfb-0.2/sidebar-beers.php
@@ -14,7 +14,7 @@
 							the_title('<h2 class="light beer-name beer-block">', '</h2>');
 							echo '<h3 class="light beer-tagline">' . get_post_meta( $id, '_cartogram_short_description_value', TRUE ) . '</h3>';
                             echo '<div class="back-logo big-icon multi-svg">';
-                            echo '<svg class="icon--large"><use xlink:href="#' . $post->post_name . '"></use</svg>';
+                            echo '<svg class="centered icon--large"><use xlink:href="#' . $post->post_name . '"></use</svg>';
 							echo '</div>';
 							the_content();
 						?>
