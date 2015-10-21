b9a042608b51500d116778455df03fc2dc1a188c
Matt Seccafien <matt@studiocartogram.com>
2015-10-21 17:28:22 -0400

    added new sidebar styles

--- wp-content/themes/wptheme-lfb-0.2/sidebar-contact-us.php
+++ wp-content/themes/wptheme-lfb-0.2/sidebar-contact-us.php
@@ -63,43 +63,43 @@
 	</div>
 </div>		
 <h3 class="rule-left text-center ">Visit Us</h3>
-<div class="row collapse flushed-right">
-    <div class="columns twelve double-bordered">
-        <ul class="list-contact ">
-            <li class="row collapse bg-cream">
-            <a href="http://maps.google.com/?q=<?php the_field('address_line_1', 'option'); ?><?php the_field('address_line_2', 'option'); ?> ">	
-                    <div class="columns rule-left mobile-one three bg-navy border-bottom-white">
-                        <h4 class="text-center text-tall light">Location</h4>
-                    </div>
-                    <div class="columns mobile-three nine format-text ">
-                        <h5><?php the_field('address_line_1', 'option'); ?><br/><?php the_field('address_line_2', 'option'); ?></h5>
-                    </div>
-                </a>
-            </li>
-            <li class="row collapse bg-cream">
-                    <div class="columns rule-left mobile-one three bg-navy border-bottom-white">
-                        <h4 class="text-center text-tall light">Hours</h4>
-                    </div>
-                    <div class="columns mobile-three nine format-text ">
-                        <h5><?php the_field('hours', 'option'); ?></h4>
-                    </div>
-            </li>
-        </ul>
-    </div>
-</div>
+<!-- <div class="row collapse flushed&#45;right"> -->
+<!--     <div class="columns twelve double&#45;bordered"> -->
+<!--         <ul class="list&#45;contact "> -->
+<!--             <li class="row collapse bg&#45;cream"> -->
+<!--             <a href="http://maps.google.com/?q=<?php the_field('address_line_1', 'option'); ?><?php the_field('address_line_2', 'option'); ?> ">	 -->
+<!--                     <div class="columns rule&#45;left mobile&#45;one three bg&#45;navy border&#45;bottom&#45;white"> -->
+<!--                         <h4 class="text&#45;center text&#45;tall light">Location</h4> -->
+<!--                     </div> -->
+<!--                     <div class="columns mobile&#45;three nine format&#45;text "> -->
+<!--                         <h5><?php the_field('address_line_1', 'option'); ?><br/><?php the_field('address_line_2', 'option'); ?></h5> -->
+<!--                     </div> -->
+<!--                 </a> -->
+<!--             </li> -->
+<!--             <li class="row collapse bg&#45;cream"> -->
+<!--                     <div class="columns rule&#45;left mobile&#45;one three bg&#45;navy border&#45;bottom&#45;white"> -->
+<!--                         <h4 class="text&#45;center text&#45;tall light">Hours</h4> -->
+<!--                     </div> -->
+<!--                     <div class="columns mobile&#45;three nine format&#45;text "> -->
+<!--                         <h5><?php the_field('hours', 'option'); ?></h4> -->
+<!--                     </div> -->
+<!--             </li> -->
+<!--         </ul> -->
+<!--     </div> -->
+<!-- </div> -->
+<div class="visit-us-section">
 <?php if( have_rows('notes', 'option') ): ?>
-
-
-<div class="row collapse flushed-right">
-    <div class="visit-us-section columns twelve bg-cream ">
     <?php while( have_rows('notes', 'option') ): the_row(); ?>
-            <div class="visit-us rule-left flush">
-                <h4 class="i-block text-center bg-navy soft-quarter text-cream visit-us__label light"><?php the_sub_field('title'); ?></h4>
-                <p class="soft-half soft-quarter--top flush "><?php the_sub_field('text'); ?></p>
+        <div class="visit-us">
+           <div class="row collapse bg-cream  flushed-right "> 
+               <div class="columns visit-us__label mobile-one three bg-navy ">
+                    <h4 class="text-center light"><?php the_sub_field('title') ?></h4>
+                </div>
+                <div class="columns mobile-three nine format-text ">
+                    <p class="soft-half soft-quarter--top flush"><?php the_sub_field('text'); ?></p>
+                </div>
             </div>
+        </div>
     <?php endwhile; ?>
-
-
-    </div>
-</div>
 <?php endif; ?>
+</div>
