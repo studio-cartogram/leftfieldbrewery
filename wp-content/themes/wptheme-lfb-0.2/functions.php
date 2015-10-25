d0a581843c0662f9f9fa7111b651ee0b4155409b
Matt Seccafien <matt@studiocartogram.com>
2015-10-24 21:00:56 -0400

    added wider website

--- wp-content/themes/wptheme-lfb-0.2/functions.php
+++ wp-content/themes/wptheme-lfb-0.2/functions.php
@@ -79,7 +79,7 @@
 		wp_register_script( 'app', get_template_directory_uri().'/dist/scripts/v1.js', array('jquery') );
 		wp_register_script( 'main', get_template_directory_uri().'/dist/scripts/main.js', array(), false, true);
         wp_enqueue_script( 'app', array('jquery')  );
-        wp_enqueue_script('main');
+    if(is_page('brew-finder')) {    wp_enqueue_script('main'); };
 		/* Give index-ck.js access to the variable AjaxResources containg the given info in the array */
 		wp_localize_script('app', 'AjaxResources', array('ajax_url'=> get_site_url() . '/wp-admin/admin-ajax.php',
 		'post_id' => get_the_ID(), 'request_url' => get_permalink(get_the_ID())));
