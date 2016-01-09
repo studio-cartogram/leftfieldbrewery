216f02e6cd747c75f176ce06fb5988368c40bad2
Matt Seccafien <matt@studiocartogram.com>
2016-01-09 12:31:17 +0700

    added loader

--- wp-content/themes/wptheme-lfb-0.2/parts/content/content-brew-finder.php
+++ wp-content/themes/wptheme-lfb-0.2/parts/content/content-brew-finder.php
@@ -5,7 +5,9 @@
 <div class="row  ">
     <div class="columns twelve">
         <?php the_title('<h3 class="rule-left rule-right text-center">','</h3>') ?>
-        <div class="brew-finder" id="map"></div>
+        <div class="brew-finder" id="map">
+            <?php get_template_part('parts/shared/loader');?>
+        </div>
     </div>
 </div>
 </div>
---
+++ wp-content/themes/wptheme-lfb-0.2/parts/shared/loader.php
@@ -0,0 +1,6 @@
+<div class="loader">
+    <span class="base base--first"></span>
+    <span class="base base--second"></span>
+    <span class="base base--third"></span>
+    <span class="base base--home"></span>
+</div>
