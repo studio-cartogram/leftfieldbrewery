18aa2524855b4b342f10937f3c7d03b651beae09
Matt Seccafien <matt@studiocartogram.com>
2016-03-07 00:21:19 +0700

    -

--- wp-content/themes/wptheme-lfb-0.2/parts/content/content-instagram.php
+++ wp-content/themes/wptheme-lfb-0.2/parts/content/content-instagram.php
@@ -1,5 +1,5 @@
 <div class="image-wrap">
-	<?php the_post_thumbnail(); ?>
+	<?php the_post_thumbnail('full'); ?>
 	<div class="image-caption">
 		<?php the_title() ?> <?php echo get_the_content() ?>
 	</div>
