ade3949374ce5ce3baf2fc91098fcf0e41070e67
feeohnah <fiona@studiocartogram.com>
2018-05-15 13:50:34 -0400

    adjusted the bbeer sidebars to be auto-height

--- wp-content/themes/wptheme-lfb-0.2/sidebar-beers.php
+++ wp-content/themes/wptheme-lfb-0.2/sidebar-beers.php
@@ -19,29 +19,24 @@ $bottleshop = get_field('bottle_shop');
 <div class="row collapse flushed-right sidebar-beer">
 	<div class="columns twelve double-bordered">
 		<div class="rule-left">
-			<div style="background:<?php echo $color; ?>" class="flip-container ">
-				<a class="flip" id="to-back" href="#"><i style="color:<?php echo $color; ?>"  class="icon-flip-right"></i></a>
-				<div class="flipper">
-					<div class="front" style="background:<?php echo $color; ?>">
-						<span class="corner-two "></span>
-							<?php
-							the_title('<h2 class="light beer-name beer-block">', '</h2>');
-							echo '<h3 class="light beer-tagline">' . $short_description . '</h3>';
-              echo '<div class="back-logo big-icon multi-svg">';
-              echo '<img src="' . wp_get_attachment_image_src( $icon_image )[0] . '" />';
-							echo '</div>';
-							the_content();
-						?>
-					</div>
-				</div>
+			<div class="front flip-container" style="background:<?php echo $color; ?>">
+				<span class="corner-two "></span>
+					<?php
+					the_title('<h2 class="light beer-name beer-block">', '</h2>');
+					echo '<h3 class="light beer-tagline">' . $short_description . '</h3>';
+					echo '<div class="back-logo big-icon multi-svg">';
+					echo '<img src="' . wp_get_attachment_image_src( $icon_image )[0] . '" />';
+					echo '</div>';
+					the_content();
+				?>
 			</div>
 			<?php 
-					if ($bottleshop):
-						echo '<div class="beer-bottleshop">';
-							echo '<a href="'. $bottleshop .'" class="link--bordered link--brew-finder  link--callout-module">Buy this beer online</a>';
-						echo '</div>';
-					endif;
-					?>
+				if ($bottleshop):
+					echo '<div class="beer-bottleshop">';
+						echo '<a href="'. $bottleshop .'" class="link--bordered link--brew-finder  link--callout-module">Buy this beer online</a>';
+					echo '</div>';
+				endif;
+			?>
 		</div>
 	</div>
 </div>
