d82aad0d5b5a275d63c2143ba77b8c441293f592
Matt Seccafien <matt@studiocartogram.com>
2019-11-23 01:02:36 -0500

    fix newsletter form

--- wp-content/themes/wptheme-lfb-0.2/parts/newsletter.php
+++ wp-content/themes/wptheme-lfb-0.2/parts/newsletter.php
@@ -4,8 +4,10 @@ $newsletter_text_field = get_field('newsletter_text', 'options');
 $newsletter_button_text_field = get_field('newsletter_button_text', 'options');
 $newsletter_text = $newsletter_text_field ? $newsletter_text_field : "Sign up to our Clubhouse Report<br/> for brewery news and events.";
 $newsletter_button_text = $newsletter_button_text_field ? $newsletter_button_text_field : "Sign up";
+$newsletter_url = "https://leftfieldbrewery.us6.list-manage.com/subscribe/post?u=4d27eaba84&amp;id=82ab30d4fa";
+$secret_html = '<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_4d27eaba84_82ab30d4fa" tabindex="-1" value=""></div>';
 
-echo '<form class="newsletter" action="//fondfolio.us13.list-manage.com/subscribe/post?u=65e81a3fa309924ed7ff3fd68&amp;id=fb444d9189" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" target="_blank">';
+echo '<form class="newsletter" action="' . $newsletter_url . '" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" target="_blank">';
 
   echo '<div>';
 
@@ -14,6 +16,8 @@ echo '<form class="newsletter" action="//fondfolio.us13.list-manage.com/subscrib
   echo '</div>';
 
   echo '<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_65e81a3fa309924ed7ff3fd68_fb444d9189" tabindex="-1" value=""></div>';
+
+  echo $secret_html;
 
   echo '<div class="combined-input">';
 
