7d9e075a1300f5891637150a03040e6fc795ee51
Matt Seccafien <matt@studiocartogram.com>
2019-12-02 22:52:07 -0500

    more events

--- wp-content/themes/wptheme-lfb-0.2/archive-events.php
+++ wp-content/themes/wptheme-lfb-0.2/archive-events.php
@@ -1,20 +1,21 @@
 <?php
+
 /**
  * Template Name: Home
  */
+
+$slug = 'events';
 
 get_template_part('parts/shared/html-header');
 
 get_header();
 
-echo '<div class="container" id="container-site">';
-	
-	get_template_part('partials/page', 'events');
+echo '<div class="container ' . $slug  . '" id="container-site">';
+
+get_template_part('partials/page', $slug);
 
 echo '</div>';
 
 get_footer();
 
 get_template_part('parts/shared/html-footer');
-
-?>
