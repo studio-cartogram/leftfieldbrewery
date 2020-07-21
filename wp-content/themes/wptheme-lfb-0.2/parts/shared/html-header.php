15c53dec7e19aba85362f79f00c57a1e48e2cff1
feeohnah <fiona@studiocartogram.com>
2020-07-21 14:46:59 -0400

    favicon

--- wp-content/themes/wptheme-lfb-0.2/parts/shared/html-header.php
+++ wp-content/themes/wptheme-lfb-0.2/parts/shared/html-header.php
@@ -13,6 +13,13 @@
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 		<link rel="icon" type="image/svg+xml" href="/icon.svg"/>
 		<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
+		<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+		<link rel="manifest" href="/site.webmanifest">
+		<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#0c223f">
+		<meta name="msapplication-TileColor" content="#0c223f">
+		<meta name="theme-color" content="#ffffff">
 
 		<meta property="og:title" content="Left Field Brewery"/>
 		<meta property="og:type" content="website" />
