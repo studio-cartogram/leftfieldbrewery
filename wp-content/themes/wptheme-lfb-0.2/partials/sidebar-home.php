b8f3e7bc85be3e4b7d8dcdeec6f6781bd615cc13
Matthew Seccafien <matt.seccafien@shopify.com>
2020-07-21 22:01:18 -0400

    add text to callout modules

--- wp-content/themes/wptheme-lfb-0.2/partials/sidebar-home.php
+++ wp-content/themes/wptheme-lfb-0.2/partials/sidebar-home.php
@@ -5,16 +5,21 @@ if (have_rows('callout')) :
   while (have_rows('callout')) : the_row();
     $heading = get_sub_field('callout_heading');
     $image = get_sub_field('callout_image');
-    $text = get_sub_field('callout_text');
     $link = get_sub_field('callout_link');
+    $text = get_sub_field('callout_text') or $link;
+    $content = get_sub_field('callout_content');
 
-    echo '<span class="heading heading--3 soft text-center">' . $heading . '</span>';
+    echo $heading ? '<span class="heading heading--3 soft text-center">' . $heading . '</span>' : '';
+    echo $content ? '<article class="callout__content bg-navy cream"><span class="heading--5">' . $content . '</span></article>' : '';
 
     echo '<section style="background-image: url(' . $image . ');" class="callout__inner bg-cream">';
-      echo '<a href="' . $link . '" class="link--bordered link--brew-finder link--callout-module">';
-        echo $text;
-      echo '</a>';
+      if ($link) :
+        echo '<a href="' . $link . '" class="link--bordered link--brew-finder link--callout-module">';
+          echo $text;
+        echo '</a>';
+      endif;
     echo '</section>';
+
     
   endwhile;
 endif;
